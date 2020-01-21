48872df551d87c3b09dc1ffff5e0d4846f9b54bb
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 21:12:30 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,16 @@
-print("hello")
-print("world")
-print("hello")
-print()
+def __str__(self) -> str:
+   """ Return a string representation of this Member.
+   member = Member('Paul', 'Ajax', 'pgries@cs.trontoedu')
+   member.__str__()
+   'Paul\\nAjax\\npgries@cs.tronto.edu'
+   """
+    
+    return '{}\n{}\n{}'.format(self.name, self.address, self.email)
+   
+    paul = Faculty('Paul','Ajax','pgries@cs.tronto.edu', '1234')
+    str(paul)
+    'paul\nAjax\npgries@cs.tronto.edu'
+    print(paul)
+    Paul
+    Ajax
+    pgries@cs.tronto.edu
